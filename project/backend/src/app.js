1c50565c8aa397815be135017f1f37a4fd5eab08
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-06-09 14:09:36 -0300

    [feat] Criar lógica de conexão com database

---
+++ project/backend/src/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { sequelize } = require('./models');
+
+async function startServer() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+
+        await sequelize.sync({ force: false }); // Impedir a recriação de tabelas e exclusão de dados
+        console.log('Modelos sincronizados com o banco de dados.');
+
+        /* 
+        Rotas e controllers
+        */
+        console.log('Servidor iniciado.');
+    } catch (error) {
+        console.error('Erro ao conectar ao banco de dados:', error);
+    }
+}
+
+startServer();
