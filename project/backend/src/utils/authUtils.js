bdc8a239f0e97557d4327bd225c51f4b7ee80463
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-06-09 19:18:54 -0300

    [feat] Criar lógica de criptografia de senha

---
+++ project/backend/src/utils/authUtils.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+async function hashPassword(password) {
+    try {
+        const hash = await bcrypt.hash(password, saltRounds);
+        return hash;
+    } catch (error) {
+        throw new Error('Erro ao hashear a senha: ' + error.message);
+    }
+}
+
+async function verifyPassword(password, hash) {
+    try {
+        const match = await bcrypt.compare(password, hash);
+        return match;
+    } catch (error) {
+        throw new Error('Erro ao verificar a senha: ' + error.message);
+    }
+}
+
+module.exports = {
+    hashPassword,
+    verifyPassword,
+};
