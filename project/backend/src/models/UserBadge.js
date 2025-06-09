1f5d2fb2d180cc586d00400ecec568ef3e2a70d6
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-06-08 21:41:06 -0300

    [feat] Criar modelo de tabelas associativas para usuários e entidades

---
+++ project/backend/src/models/UserBadge.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const UserBadge = sequelize.define('UserBadge', {
+        isVisibleOnProfile: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true,
+            field: 'is_visible_on_profile',
+        },
+    }, {
+        tableName: 'user_badge',
+        timestamps: true,
+        createdAt: 'granted_at',
+        updatedAt: 'false', 
+    });
+}
