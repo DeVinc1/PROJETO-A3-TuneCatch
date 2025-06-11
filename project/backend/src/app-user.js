17b07beedca50a0090057d0866f184623ecf4295
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-06-11 15:23:19 -0300

    [refactor] Aumentar clareza a respeito da responsabilidade do serviço

--- project/backend/src/app-user.js
+++ project/backend/src/app-user.js
@@ -12,25 +12,25 @@ const PORT = process.env.USER_PORT;
 async function startServer() {
     try {
         await sequelize.authenticate();
-        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+        console.log('[User Service] Conexão com o banco de dados estabelecida com sucesso.');
 
         await sequelize.sync({ force: false }); // Impedir a recriação de tabelas e exclusão de dados
-        console.log('Modelos sincronizados com o banco de dados.');
+        console.log('[User Service] Modelos sincronizados com o banco de dados.');
 
         app.use('/maestro', userRoutes);
   
         app.get('/', (req, res) => {
-            res.send('Bem-vindo à API Maestro do TuneCatch! 🎶');
+            res.send('[User Service] Bem-vindo à API Maestro de Usuários do TuneCatch! 🎶');
         });
 
         app.use(errorHandler);
 
         app.listen(PORT, () => {
-            console.log(`🚀 Servidor iniciado rodando na porta ${PORT}`);
+            console.log(`🚀 Servidor de Usuários iniciado e rodando na porta ${PORT}`);
         });
 
     } catch (error) {
-        console.error('Erro ao conectar ao banco de dados:', error);
+        console.error('[User Service] Erro ao conectar ao banco de dados:', error);
     }
 }
 
