efbf326fc88b045ccf622b7fc80803cf3f697d24
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-06-15 18:43:12 -0300

    [fix] Configurando rotas e scripts corretamente

--- project/frontend/src/main.jsx
+++ project/frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import { AuthProvider } from './contexts/AuthContext.jsx'; // Importe o AuthProvider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <App />
+    <BrowserRouter>
+      <AuthProvider> {/* Envolve toda a aplicação com o AuthProvider */}
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
