e706260819189ff0eda5e182b5c292e3638e8653
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-06-16 14:54:37 -0300

    [fix] Remover páginas obsoletas

--- project/frontend/src/routes/AppRoutes.jsx
+++ project/frontend/src/routes/AppRoutes.jsx
@@ -13,9 +13,6 @@ import PlaylistPage from '../pages/PlaylistPage.jsx';
 
 import GeneralSearchPage from '../pages/GeneralSearchPage.jsx';      
 import SearchByTagPage from '../pages/SearchByTagPage.jsx';          
-import SearchByUsernamePage from '../pages/SearchByUsernamePage.jsx'; 
-import SearchByPlaylistNamePage from '../pages/SearchByPlaylistNamePage.jsx';
-import SearchTracksInPlaylistPage from '../pages/SearchTracksInPlaylistPage.jsx'; 
 import PlaylistsFromUser from '../pages/PlaylistsFromUser.jsx'; 
 
 import NotFoundPage from '../pages/NotFoundPage.jsx';
@@ -32,8 +29,6 @@ function AppRoutes() {
       {/* Rotas de Pesquisa Detalhadas */}
       <Route path="/search" element={<GeneralSearchPage />} /> 
       <Route path="/search/tags/:tagQuery?" element={<SearchByTagPage />} />
-      <Route path="/search/users/:usernameQuery?" element={<SearchByUsernamePage />} /> 
-      <Route path="/search/playlists/:playlistNameQuery?" element={<SearchByPlaylistNamePage />} /> 
 
       {/* Rotas do perfil logado */}
       <Route path="/liked-playlists" element={<LikedPlaylistsPage />} />
@@ -44,9 +39,6 @@ function AppRoutes() {
       <Route path="/users/:userId" element={<UserProfilePage />} /> 
       <Route path="/playlist/:playlistId" element={<PlaylistPage />} /> \
       <Route path="/playlists-from/:id" element={<PlaylistsFromUser />} />
-
-      {/* Rota para buscar playlists que possuam uma música */}
-      <Route path="/playlist/search-tracks/:trackName" element={<SearchTracksInPlaylistPage />} />
 
       {/* Rota 404 (catch-all para qualquer caminho não definido) */}
       <Route path="*" element={<NotFoundPage />} />
