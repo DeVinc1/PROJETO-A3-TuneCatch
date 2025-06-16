882e95949fa4c07abd55f3e554214723f447c92f
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-06-16 11:05:03 -0300

    [style] Retirar degradê

--- project/frontend/src/components/TopBackground.jsx
+++ project/frontend/src/components/TopBackground.jsx
@@ -6,7 +6,7 @@ function TopBackground({ height }) {
       className="fixed top-0 left-0 right-0 z-0" 
       style={{
         height: height, 
-        background: 'linear-gradient(to bottom, #FFF9F9, rgba(255, 249, 249, 0.8))', 
+        background: 'linear-gradient(to bottom, #FFF9F9, rgba(255, 249, 249, 1))', 
       }}
     ></div>
   );
